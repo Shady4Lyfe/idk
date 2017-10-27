e062d1bcde9da51f2a66aa31e35c4a4090cf3178
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-10-26 21:14:09 -0600

    dwa

--- sketch.js
+++ sketch.js
@@ -10,24 +10,23 @@ function setup() {
 
 function draw() {
     background(66, 183, 254);
-    
+
     fill(100);
     rectMode(CENTER);
-    rect(xRot(newA), yRot(newA), random(25, 100), random(25, 100)); 
+    rect(xRot(newA), yRot(newA), random(25, 100), random(25, 100));
     newA -= 0.05;
     rad = 80;
 
-    
+
 
 }
 
 function xRot(a) {
-    var newXRot = width/2 + cos(a) * radius;
+    var newXRot = width / 2 + cos(a) * radius;
     return newXRot;
 }
 
 function yRot(a) {
-    var newYRot = height/2 + sin(a) * radius;
+    var newYRot = height / 2 + sin(a) * radius;
     return newYRot;
 }
-
