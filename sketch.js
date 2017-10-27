8f43645ce82bff1ded3399c635b6dd7efe01d3af
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-10-27 11:58:36 -0600

    pls do this at home i beg you

--- sketch.js
+++ sketch.js
@@ -4,30 +4,34 @@ var radius;
 function setup() {
     createCanvas(1200, 800);
     newA = 0;
-    radius = 80;
+    radius = 300;
 }
 
 
 function draw() {
-    background(66, 183, 254);
-    
-    fill(100);
-    rectMode(CENTER);
-    rect(xRot(newA), yRot(newA), random(25, 100), random(25, 100)); 
-    newA -= 0.05;
-    rad = 80;
+    background(66, 183, 255);
+    ellipse(xRot(newA), yRot(newA), 75, 75);
+    ellipse(xRot(-newA), yRot(-newA), 75, 75);
+    rectMode(CORNER)
+    fill(13, 255, 105, 150)
+    rect(0, 600, innerWidth, innerHeight)
+    fill(255, 255, 0, 200);
 
-    
+
+
+
+    newA += 0.05;
+
 
 }
 
+
 function xRot(a) {
-    var newXRot = width/2 + cos(a) * radius;
+    var newXRot = width / 2 + cos(a) * radius;
     return newXRot;
 }
 
 function yRot(a) {
-    var newYRot = height/2 + sin(a) * radius;
+    var newYRot = height / 2 + sin(a) * radius;
     return newYRot;
 }
-
