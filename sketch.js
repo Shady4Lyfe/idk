a6688fd4b112a0c3233bece628cdb5ef7090f537
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-10-30 11:59:57 -0600

    hehe xd

--- sketch.js
+++ sketch.js
@@ -4,21 +4,26 @@ var radius;
 function setup() {
     createCanvas(1200, 800);
     newA = 0;
-    radius = 300;
+    radius = 6;
 }
 
 
 function draw() {
+    // DRAW
+    background(66, 183, 255); // Background
+    fill(13, 255, 105); // Ground green
+    rect(0, 600, innerWidth, innerHeight); // Ground
+    
+    fill(96, 71, 31); // Brown
+    rect(width / 6, height / 2 - 80, 65, 280); // Tree trunk
 
-    background(66, 183, 255);
-    ellipse(xRot(newA), yRot(newA), 75, 75);
-    ellipse(xRot(-newA), yRot(-newA), 75, 75);
-    rectMode(CORNER)
-    fill(13, 255, 105, 150)
-    rect(0, 600, innerWidth, innerHeight)
-    fill(255, 255, 0, 200);
-
-
+    fill(28, 191, 60); // Leaf green
+    ellipse(233, height / 2 - 80, 100, 100); // Static leaf
+    ellipse(xRot(newA) - 40, yRot(newA), 100, 100); //  Left leaf
+    ellipse(xRot(-newA) + 40, yRot(-newA), 100, 100); // Right leaf
+    
+    
+    // LOGIC
     newA += 0.05;
 
 
@@ -26,11 +31,11 @@ function draw() {
 
 
 function xRot(a) {
-    var newXRot = width / 2 + cos(a) * radius;
+    var newXRot = 233 + cos(a) * radius;
     return newXRot;
 }
 
 function yRot(a) {
-    var newYRot = height / 2 + sin(a) * radius;
+    var newYRot = height / 2 - 20 + sin(a) * radius;
     return newYRot;
 }
