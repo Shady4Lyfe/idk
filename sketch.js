53ddc2b8efe23c1f78fa43ae38ba53ecdf39c187
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-10-31 12:08:18 -0600

    mhm

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 var newA;
 var radius;
+var r;
+var g;
+var b;
 
 function setup() {
     createCanvas(1200, 800);
     newA = 0;
-    radius = 6;
+    radius = 5;
+    r = 66;
+    g = 210;
+    b = 255;
 }
 
 
 function draw() {
     // DRAW
-    background(66, 183, 255); // Background
-    fill(13, 255, 105); // Ground green
+    background(r, g, b); // Background
+    fill(13, 225, 105); // Ground green
     rect(0, 600, innerWidth, innerHeight); // Ground
-    
+
     fill(96, 71, 31); // Brown
     rect(width / 6, height / 2 - 80, 65, 280); // Tree trunk
 
@@ -21,21 +27,36 @@ function draw() {
     ellipse(233, height / 2 - 80, 100, 100); // Static leaf
     ellipse(xRot(newA) - 40, yRot(newA), 100, 100); //  Left leaf
     ellipse(xRot(-newA) + 40, yRot(-newA), 100, 100); // Right leaf
-    
-    
-    // LOGIC
-    newA += 0.05;
 
+
+    // LOGIC
+    newA += 0.05; // Continuously increase new angle
+
+    for(let x = )
 
 }
 
 
+// Formulas that constantly rotate x and y in a circle
 function xRot(a) {
-    var newXRot = 233 + cos(a) * radius;
+    var newXRot = 233 + cos(a) * radius; // (x) + cos(angle) * radius
     return newXRot;
 }
 
 function yRot(a) {
-    var newYRot = height / 2 - 20 + sin(a) * radius;
+    var newYRot = height / 2 - 20 + sin(a) * radius; // (y) + sin(angle) * radius
     return newYRot;
+}
+
+// Toggle background color
+function keyPressed() {
+    if (keyCode == 38) {
+        r = 66;
+        g = 210;
+        b = 255;
+    } else if (keyCode == 40) {
+        r = 15;
+        g = 0;
+        b = 75;
+    }
 }
