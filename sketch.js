2c64c57af3201b564fa4b0073ff5c7bc21761e1b
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-11-02 11:37:54 -0600

    iloveu

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 // Animation
 
-var newA;
+var newAngle;
 var radius;
 var r;
 var g;
 var b;
+var randArray = [];
 
 function setup() {
     createCanvas(1200, 800);
-    newA = 0;
-    radius = 3;
+    newAngle = 0;
+    radius = 5;
     r = 66;
     g = 210;
     b = 255;
+    for (var i = 0; i < width; i++) { // Allows to loop the push
+        randArray.push(random(560, 600)); // Pushes numbers into array
+    }
 }
 
 
 function draw() {
     // DRAW
     background(r, g, b); // Background
+    stroke(0);
     fill(13, 225, 105); // Ground green
     rect(0, 600, innerWidth, innerHeight); // Ground
 
@@ -27,14 +32,19 @@ function draw() {
 
     fill(28, 191, 60); // Leaf green
     ellipse(233, height / 2 - 80, 100, 100); // Static leaf
-    ellipse(xRot(newA) - 40, yRot(newA), 100, 100); //  Left leaf
-    ellipse(xRot(-newA) + 40, yRot(-newA), 100, 100); // Right leaf
+    ellipse(xRot(newAngle) - 40, yRot(newAngle), 100, 100); //  Left leaf
+    ellipse(xRot(-newAngle) + 40, yRot(-newAngle), 100, 100); // Right leaf
 
     fill(100); // Grey
     quad(700, 525, 800, 500, 825, 600, 650, 600); // Rock
 
     // LOGIC
-    newA += 0.04; // Continuously increase new angle
+    newAngle += 0.03; // Continuously increase new angle
+
+    for (let x = 0; x <= innerWidth; x++) {
+        stroke(154, 255, 86); // Grass green
+        line(x, 600, x + sin(random(135, 225)), randArray[x]); // x₂ gives a "swaying" effect
+    }
 
 }
 
