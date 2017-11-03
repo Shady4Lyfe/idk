d7d98109ffc60d42c5b816d4ab7dfd72adbfad5c
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-11-03 11:20:02 -0600

    :)

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-// Landscape
+// Windy Day
 
 var newAngle;
 var radius;
 var r;
 var g;
 var b;
+var x1;
+var x2;
+var x3;
+var x4;
 var randArray = [];
 
 function setup() {
@@ -14,6 +18,10 @@ function setup() {
     r = 66;
     g = 210;
     b = 255;
+    x1 = 700;
+    x2 = 800;
+    x3 = 825;
+    x4 = 650;
     
     for (var i = 0; i < width; i++) {
         randArray.push(random(560, 600)); // Pushes numbers into array
@@ -25,7 +33,7 @@ function draw() {
     // DRAW
     background(r, g, b);
     stroke(0);
-    
+
     fill(13, 225, 105); // Ground green
     rect(0, 600, innerWidth, innerHeight); // Ground
 
@@ -38,14 +46,42 @@ function draw() {
     ellipse(xRot(-newAngle) + 40, yRot(-newAngle), 100, 100); // Right leaf
 
     fill(100); // Grey
-    quad(700, 525, 800, 500, 825, 600, 650, 600); // Rock
+    quad(x1, 525, x2, 500, x3, 600, x4, 600); // Rock
 
     // LOGIC
     newAngle += 0.03; // Continuously increase new angle
 
+    if (keyIsDown(39)) { // If right arrow key is down, rock moves right
+        x1 += 1;
+        x2 += 1;
+        x3 += 1;
+        x4 += 1;
+    }
+
+    if (keyIsDown(37)) { // If left arrow key is down, rock moves left
+        x1 -= 1;
+        x2 -= 1;
+        x3 -= 1;
+        x4 -= 1;
+    }
+
+    if (x3 >= innerWidth - 80) { // Rock will stop when touching edge of screen
+        x1 -= 1;
+        x2 -= 1;
+        x3 -= 1;
+        x4 -= 1;
+    }
+
+    if (x4 <= width / 6 + 65) { // Rock will stop when touching tree
+        x1 += 1;
+        x2 += 1;
+        x3 += 1;
+        x4 += 1;
+    }
+
     for (let x = 0; x <= innerWidth; x++) {
         stroke(154, 255, 86); // Grass green
-        line(x, 600, x + sin(random(135, 225)), randArray[x]); // x₂ gives a "swaying" effect
+        line(x, 600, x + sin(random(135, 225)), randArray[x]); // x₂ gives a "swaying" effect, courtesy of Shunyu Zhao
     }
 
 }
